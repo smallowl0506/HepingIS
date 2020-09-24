b74a4d3b1b41d75ff69227d66be8f3f747cf999e
smallowl0506 <smallowl0506@gmail.com>
2020-09-24 23:03:33 +0800

    從遠古到近世新增卷軸下拉提示

--- 從遠古到近世/test_RWD/script.js
+++ 從遠古到近世/test_RWD/script.js
@@ -1,3 +1,11 @@
-$(".backtoindex").click(function () {
-  window.history.back();
+$( document ).ready(function() {
+	$(".backtoindex").click(function () {
+		window.history.back();
+    });
+	var s = skrollr.init({
+		mobileCheck: function() {
+		  return false;
+		}
+	});
+	console.log("skrollr is initial!!!")
 });
