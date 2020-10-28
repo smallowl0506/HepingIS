76caff3e24fd99fdeefb6219d9190dba6c6da65c
micua <dollar29147@gmail.com>
2020-10-28 18:15:01 +0800

    1028

--- 計畫資訊室_專題/script.js
+++ 計畫資訊室_專題/script.js
@@ -13,7 +13,6 @@ $(".backtoindex").click(function () {
   window.history.back();
 });
 
-<<<<<<< HEAD
 var swiper = new Swiper('.swiper-container', {
   spaceBetween: 30,
   pagination: {
@@ -21,29 +20,3 @@ var swiper = new Swiper('.swiper-container', {
     clickable: true,
   },
 });
-=======
-$(function(){
-  AOS.init();
-})
-
-$("#flip").click(function(){
-  $("#p").slideToggle("slow");
-});
-
-$("#flip2").click(function(){
-  $("#p2").slideToggle("slow");
-});
-
-$("#flip3").click(function(){
-  $("#p3").slideToggle("slow");
-});
-
-$("#flip4").click(function(){
-  $("#p4").slideToggle("slow");
-});
-
-$("#flip5").click(function(){
-  $("#p5").slideToggle("slow");
-});
-
->>>>>>> 10fdbdbfb6d29db6dab312d11943e46409b6e57e
