92d005368744c5599fb0386f963e9e8f28a344e7
micua <dollar29147@gmail.com>
2020-10-28 17:56:54 +0800

    1028

---
+++ 計畫資訊室_專題/script.js
@@ -0,0 +1,22 @@
+var percent = 0
+
+var timer = setInterval(function(){
+  $(".bar").css("width",percent+"%")
+  percent+=0.5
+  if (percent>102){
+    $(".pageLoading").addClass("complete")
+    clearInterval(timer)
+  }
+},20)
+
+$(".backtoindex").click(function () {
+  window.history.back();
+});
+
+var swiper = new Swiper('.swiper-container', {
+  spaceBetween: 30,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
