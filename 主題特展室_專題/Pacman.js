353928ab26fcc8b84d5fcaa64efd9d6df4a2a6ec
smallowl0506 <smallowl0506@gmail.com>
2020-11-01 14:43:51 +0800

    修改專題版本主題特展室

--- 主題特展室_專題/Pacman.js
+++ 主題特展室_專題/Pacman.js
@@ -2,20 +2,20 @@ class PacMan {
   constructor(x,y){
     this.pos = createVector(x,y)
     this.vel = createVector(0,0)
-    this.dir = floor(random(0, 5))
+    this.dir = floor(random(0, 4))
     this.angle = this.dir * HALF_PI
-    this.radius = random(40,50)
-    this.color = color(random(255),random(255),random(255))
-    this.index = floor(random(99999))
-    this.isOutOfEdge = false
-    this.isInitial = true
-    this.isKilled = false
+    this.radius = 50
+    this.color = color(255,255,0)
+    // this.index = floor(random(99999))
+    // this.isOutOfEdge = false
+    // this.isInitial = true
+    // this.isKilled = false
   }
   
   show() {
-    if(this.isKilled){
-      this.color.setAlpha(80)
-    }
+    // if(this.isKilled){
+    //   this.color.setAlpha(80)
+    // }
     fill(this.color)
     noStroke()
     let a = abs(sin(frameCount*0.2)) * 0.5
@@ -23,57 +23,62 @@ class PacMan {
   }
   
   update(){
-    if((frameCount+this.index)%100==0 || this.isInitial == true){
-      this.dir = floor(random(0,5))
+    // if((frameCount+this.index)%100==0 || this.isInitial == true){
+      // this.dir = floor(random(0,5))
       this.angle = this.dir * HALF_PI
-      switch(this.dir){
-        case 0:
-          this.vel.x = random(1,5)
-          this.vel.y = 0
-          break
-        case 1:
-          this.vel.x = 0
-          this.vel.y = random(1,5)
-          break
-        case 2:
-          this.vel.x = -random(1,5)
-          this.vel.y = 0
-          break
-        case 3:
-          this.vel.x = 0
-          this.vel.y = -random(1,5)
-          break
-        case 4:
-          this.vel.x = random(1,5)
-          this.vel.y = 0
-      }
-    }
+    //   switch(this.dir){
+    //     case 0:
+    //       this.vel.x = random(1,5)
+    //       this.vel.y = 0
+    //       break
+    //     case 1:
+    //       this.vel.x = 0
+    //       this.vel.y = random(1,5)
+    //       break
+    //     case 2:
+    //       this.vel.x = -random(1,5)
+    //       this.vel.y = 0
+    //       break
+    //     case 3:
+    //       this.vel.x = 0
+    //       this.vel.y = -random(1,5)
+    //       break
+    //     case 4:
+    //       this.vel.x = random(1,5)
+    //       this.vel.y = 0
+    //   }
+    // }
     this.outOfEdge()
     this.pos.add(this.vel)
-    if(this.isInitial){
-      this.isInitial = false
-    }
+    // if(this.isInitial){
+    //   this.isInitial = false
+    // }
   }
   
   outOfEdge(){
-    if(this.pos.x > windowWidth || this.pos.x < 0){
-      this.vel.x *=-1
-      this.angle = (this.angle+PI)%(2*PI)
-      //this.isOutOfEdge = true
+    if(this.pos.x > windowWidth){
+      this.pos.x = windowWidth
+      this.vel.x = 0
     }
-
-    if(this.pos.y > windowHeight || this.pos.y < 0){
-      this.vel.y *=-1
-      this.angle = (this.angle+PI)%(2*PI)
-      //this.isOutOfEdge = true
+    if(this.pos.x < 0){
+      this.pos.x = 0
+      this.vel.x = 0
+    }
+    if(this.pos.y > windowHeight){
+      this.pos.y = windowHeight
+      this.vel.y = 0
+    }
+    if(this.pos.y < 0){
+      this.pos.y = 0
+      this.vel.y = 0
     }
   }
   
   checkEat(food){
-     if(this.pos.x < food.pos.x + food.size/2 &&
-       this.pos.x > food.pos.x - food.size/2){
-      if(this.pos.y < food.pos.y + food.size/2 &&
-         this.pos.y > food.pos.y - food.size/2){
+     if(food.pos.x < this.pos.x + this.radius/2 &&
+       food.pos.x > this.pos.x - this.radius/2){
+      if(food.pos.y < this.pos.y + this.radius/2 &&
+         food.pos.y > this.pos.y - this.radius/2){
            food.isEaten = true
         }
      }
--- 主題特展室_專題/sketch.js
+++ 主題特展室_專題/sketch.js
@@ -1,4 +1,5 @@
-let pacmanGroup = []
+// let pacmanGroup = []
+let pacman 
 let textGroup = []
 let titleText = "主題特展室".split("")
 let titleIntro = "本廳為主題特展室，呈現各世世代代所舉辦的各式活動，以及其他重要的圖像故事等等。".split("")
@@ -7,12 +8,14 @@ let eventB = "B.XXXX活動".split("")
 let eventC = "C.XXXX活動".split("")
 let eventD = "D.XXXX活動".split("")
 let eventE = "E.XXXX活動".split("")
-let hintL = "按滑鼠左鍵生成小精靈".split("")
-let hintR = "按滑鼠右鍵重新生成文字".split("")
-let hintC = "按滑鼠中鍵清除所有小精靈".split("")
-let hintD = "按鍵盤空白鍵產生軌跡(再按一次解除)".split("")
+// let hintL = "按滑鼠左鍵生成小精靈".split("")
+// let hintR = "按滑鼠右鍵重新生成文字".split("")
+// let hintC = "按滑鼠中鍵清除所有小精靈".split("")
+// let hintD = "按鍵盤空白鍵產生軌跡(再按一次解除)".split("")
+let hint1 = "按鍵盤空白鍵(space)重新生成所有文字".split("")
+let hint2 = "本廳室所有內容尚在建置中，敬請期待!!!".split("")
 let currentFrameCount
-let hasTrail = false
+// let hasTrail = false
 
 function setup() {
   // let container = select("#myContainer");
@@ -20,18 +23,21 @@ function setup() {
   // let myCanvas = createCanvas(container.width+50, container.height);
   // myCanvas.parent('myContainer');
   createCanvas(windowWidth, windowHeight);
+  pacman = new PacMan(width/2,height/2);
   makeText()
-  canvas.oncontextmenu = () => false;  //關閉滑鼠右鍵選單
+  // canvas.oncontextmenu = () => false;  //關閉滑鼠右鍵選單
 }
 
 function draw() {
 	
-  if(hasTrail){
-    background(0,30)
-  }else{
-    background(0)
-  }
+  // if(hasTrail){
+  //   background(0,30)
+  // }else{
+  //   background(0)
+  // }
+  background(0)
   
+  //背景紋樣
   push()
   for(let i = -width; i < width; i += 20){
     for(let j = 0; j < height; j += 20){
@@ -44,63 +50,114 @@ function draw() {
   }
   pop()
   
-  for(let pacman of pacmanGroup){
-    pacman.update()
-    pacman.show()
-  }
-  for(let i = pacmanGroup.length-1;i>=0;i--){
-    if(pacmanGroup[i].isOutOfEdge == true){
-      pacmanGroup.splice(i,1)
-    }
-  }
+  //小精靈動作與呈現
+  pacman.update()
+  pacman.show()
+
+  // for(let i = pacmanGroup.length-1;i>=0;i--){
+  //   if(pacmanGroup[i].isOutOfEdge == true){
+  //     pacmanGroup.splice(i,1)
+  //   }
+  // }
+  //文字動作與呈現
   for(let text of textGroup){
     text.show()
   }
+  //檢查文字被吃掉就移除
   for(let j = textGroup.length-1;j>=0;j--){
     if(textGroup[j].isEaten == true){
       textGroup.splice(j,1)
     }
   }
-  for(let i = 0; i < pacmanGroup.length; i++){
-    for(let j = 0; j < textGroup.length; j++){
-      if(!pacmanGroup[i].isKilled && !textGroup[j].isEaten){
-        pacmanGroup[i].checkEat(textGroup[j])
-      }
+  //檢查小精靈是否有吃掉文字
+  for(let j = 0; j < textGroup.length; j++){
+   if(!textGroup[j].isEaten){
+      pacman.checkEat(textGroup[j])
     }
   }
 
 }
 
-function mousePressed(){
-  if(mouseButton == LEFT){  //如果是左鍵，生成新小精靈
-    pacmanGroup.push(new PacMan(mouseX,mouseY))
-  }
-  if(mouseButton == RIGHT){ //如果是右鍵，重新生成所有文字
-    currentFrameCount = frameCount
-    for(let j = textGroup.length-1;j>=0;j--){
-      textGroup.splice(j,1)
-    }
-    makeText()
-  }
-  if(mouseButton == CENTER){  //如果是中鍵，清除目前所有小精靈
-    for(let i = pacmanGroup.length-1;i>=0;i--){
-      pacmanGroup[i].isKilled = true
-    }
-    setTimeout(function(){
-      for(let i = pacmanGroup.length-1;i>=0;i--){
-        if(pacmanGroup[i].isKilled){
-           pacmanGroup.splice(i,1)
-        }
-      }
-    }, 3000);
-  }
-}
+// function mousePressed(){
+  // if(mouseButton == LEFT){  //如果是左鍵，生成新小精靈
+  //   pacmanGroup.push(new PacMan(mouseX,mouseY))
+  // }
+  // if(mouseButton == RIGHT){ //如果是右鍵，重新生成所有文字
+  //   currentFrameCount = frameCount
+  //   for(let j = textGroup.length-1;j>=0;j--){
+  //     textGroup.splice(j,1)
+  //   }
+  //   makeText()
+  // }
+  // }
+  // if(mouseButton == CENTER){  //如果是中鍵，清除目前所有小精靈
+  //   for(let i = pacmanGroup.length-1;i>=0;i--){
+  //     pacmanGroup[i].isKilled = true
+  //   }
+  //   setTimeout(function(){
+  //     for(let i = pacmanGroup.length-1;i>=0;i--){
+  //       if(pacmanGroup[i].isKilled){
+  //          pacmanGroup.splice(i,1)
+  //       }
+  //     }
+  //   }, 3000);
+  // }
+// }
 
 function keyPressed(){
   // print(keyCode)
-  if(keyCode==32){
-    hasTrail = !hasTrail
+  switch(keyCode){
+    //W or ↑
+    case 87:
+      pacman.dir = 3;
+      pacman.vel.y = -10;
+      break;
+     case 38:
+      pacman.dir = 3;
+      pacman.vel.y = -10;
+      break;
+    //A or ←
+    case 65:
+      pacman.dir = 2;
+      pacman.vel.x = -10;
+      break;
+    case 37:
+      pacman.dir = 2;
+      pacman.vel.x = -10;
+      break;
+    //S or ↓
+    case 83:
+      pacman.dir = 1;
+      pacman.vel.y = 10;
+      break;
+    case 40:
+      pacman.dir = 1;
+      pacman.vel.y = 10;
+      break;
+    //D or →
+    case 68:
+      pacman.dir = 0;
+      pacman.vel.x = 10;
+      break;
+    case 39:
+      pacman.dir = 0;
+      pacman.vel.x = 10;
+      break;
+    //space
+    case 32:
+      currentFrameCount = frameCount
+      for(let j = textGroup.length-1;j>=0;j--){
+        textGroup.splice(j,1)
+      }
+      makeText()
+      break;
   }
+
+}
+
+function keyReleased() {
+  pacman.vel.x = 0;
+  pacman.vel.y = 0;
 }
 
 function makeText(){
@@ -125,16 +182,10 @@ function makeText(){
   for(let i = 0 ; i<eventE.length; i++){
     textGroup.push(new Text(eventE[i],100+i*20,height/2+200,20) );
   }
-  for(let i = 0 ; i<hintL.length; i++){
-    textGroup.push(new Text(hintL[i],500+i*20,height/2+50,20) );
+  for(let i = 0 ; i<hint1.length; i++){
+    textGroup.push(new Text(hint1[i],500+i*30,height/3*2,30) );
   }
-  for(let i = 0 ; i<hintR.length; i++){
-    textGroup.push(new Text(hintR[i],500+i*20,height/2+100,20) );
-  }
-  for(let i = 0 ; i<hintC.length; i++){
-    textGroup.push(new Text(hintC[i],500+i*20,height/2+150,20) );
-  }
-  for(let i = 0 ; i<hintD.length; i++){
-    textGroup.push(new Text(hintD[i],500+i*20,height/2+200,20) );
+  for(let i = 0 ; i<hint2.length; i++){
+    textGroup.push(new Text(hint2[i],500+i*30,height/3*2+60,30) );
   }
 }
